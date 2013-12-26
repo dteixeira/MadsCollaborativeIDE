39a8b96c51b879c499dec6fb78105772ae791525
Diogo Teixeira <diogo.andre.teixeira@gmail.com>
2013-12-26 17:40:39 +0000

    Sessions bug fixed.

--- models/user.rb
+++ models/user.rb
@@ -36,12 +36,13 @@ class User
   # Validation methods.
   def self.login user
     begin
-      u = User.find(:username, user['username'])
+      u = User.first(:username, user['username'])
       u.password = user['password']
       hash = BCrypt::Engine.hash_secret u.password, u.password_salt
       u = nil unless hash == u.password_hash
       return u
-    rescue
+    rescue Exception => e
+      puts e
       return nil
     end
   end
